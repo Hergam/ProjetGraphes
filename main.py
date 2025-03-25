4b595f3cd528f436d944ecdcf70c70df266b6767
SteamMachina <lisaminne123@gmail.com>
2025-03-25 18:18:00 +0100

    création du tableau (pas fini)

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 
-numtableau = input("Enter table to test :")
-f = open("contraintes/table "+str(numtableau)+".txt", "r")
-tableau = f.read() 
+numTableau = input("Enter table to test :")
+f = open("contraintes/table "+str(numTableau)+".txt", "r")
+texte = f.readlines()
+tableau = [[]]
+for i in range(len(texte)):
+    splittedLine = texte[i].split()
+    tableau[i][0]=splittedLine[0]
+    tableau[i][1]=splittedLine[1]
+    for j in range(2,len(splittedLine)):
+        tableau[i][j]=splittedLine[j]
+
 print(tableau)
+
