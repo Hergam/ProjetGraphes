1a57d9855d61914a9a4c7f08dea5a5b447fa0b39
SteamMachina <lisaminne123@gmail.com>
2025-03-25 20:18:08 +0100

    ajouté les sommets fictifs alpha et w

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from tabulate import tabulate
 
-
-
 numTableau = input("Enter table to test :")
 f = open("contraintes/table "+str(numTableau)+".txt", "r")
 texte = f.readlines()
+texte.insert(0,'0 0\n')
 
-tableau = []
+ligneW=[]
+sommets=range(1,len(texte));
+
+for n in sommets:
+    ligneW.append(n)
+
+
 for i in range(len(texte)):
     texte[i] = texte[i].split()
+    for j in range(len(texte[i])):
+        texte[i][j]=int(texte[i][j])
+        if j>1:
+            if texte[i][j] in ligneW:
+                ligneW.remove(texte[i][j])
+
+ligneW.insert(0,len(texte))
+ligneW.insert(1,0)
+texte.append(ligneW)
 
 print(tabulate(texte, headers=['Sommet', 'Durée', '1','2','3','4']))
